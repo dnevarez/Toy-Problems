ccea32d748dfae98e2702ebf32f35fa469673c03
Dan Nevarez <dnev.dev@gmail.com>
2016-12-05 17:31:09 -0700

    chaining methods

---
+++ chainingMethods.js
@@ -0,0 +1,31 @@
+// Fix the following to allow chaining of the methods.
+// Arithmetic(6).add(9).subtract(4).add(7).val() => 18
+
+
+// My Answer
+
+(function () {
+
+   function Arithmetic(number) {
+      this.value = number;
+   }
+
+   Arithmetic.val = function () {
+      return this.value;
+   };
+
+   Arithmetic.add = function (number) {
+      this.value += number;
+   };
+
+   Arithmetic.subtract = function (number) {
+      this.value -= number;
+   };
+
+   window.Arithmetic = function (number) {
+      return new Arithmetic(number);
+   };
+
+
+
+})();
