4f3542feb4050ccbdc6f0b7b8b88cbb82084cba1
Dan Nevarez <dnev.dev@gmail.com>
2017-01-19 14:46:46 -0700

    code challenge

---
+++ javascript/codeChallenge/empireRebellion.js
@@ -0,0 +1,12 @@
+// Alphabetize by the collection by each object name
+// Return a collection of id's
+// Append " - deceased" to the end of each object title
+// Remove the first object in the collection
+// Seperate the collection into two groups. Those loyal to the empire and those to the rebellion.
+
+const collection = [
+  { id: 1, name: "Han Solo", title: "Scruffy Looking Nerf Herder", allegiance: "rebellion" },
+  { id: 2, name: "Darth Vader", title: "Sith Lord", allegiance: "empire" },
+  { id: 3, name: "K-2SO", title: "Sass-bot", allegiance: "rebellion" },
+  { id: 4, name: "Wilhuff Tarkin", title: "Grand Moff", allegiance: "empire" },
+];
