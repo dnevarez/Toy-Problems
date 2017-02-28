d546ba001421fa20af75c410ce0c3a6dabd4a178
Dan Nevarez <dnev.dev@gmail.com>
2017-02-28 13:31:01 -0700

    counting characters

---
+++ javascript/5kyu/countingChar.js
@@ -0,0 +1,17 @@
+// Create a function that takes in 3 arguments. A string (text), and number (max)
+// for the maximum character count, and a boolean (spaces), indicating whether or
+// not spaces are counted in the total of the string length.
+//
+// Return an array. The first item should be a boolean, indicating whether or not
+// string length falls within the max character count. The second should be the string.
+//
+// If the text argument is longer than the max character count. The returned string
+// should show the text up to where the max character count cuts off. The result
+// vary based on whether spaces is true or false.
+//
+// If the text argument is equal to or less than the max character count. The entire
+// text will be returned as the second item in the array.
+
+function charCount(text, max, spaces){
+  
+}
