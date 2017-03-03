170e020a00e0794db274d9a910a54875b6cbd821
Dan Nevarez <dnev.dev@gmail.com>
2017-03-03 12:14:35 -0700

    best solution

--- javascript/5kyu/alernatingSplit.js
+++ javascript/5kyu/alernatingSplit.js
@@ -1,3 +1,21 @@
+// For building the encrypted string:
+// Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
+// Do this n times!
+//
+// Examples:
+//
+// "This is a test!", 1 -> "hsi  etTi sats!"
+// "This is a test!", 2 -> "hsi  etTi sats!" -> "s eT ashi tist!"
+// Write two methods:
+//
+// function encrypt(text, n)
+// function decrypt(encryptedText, n)
+// For both methods:
+// If the input-string is null or empty return exactly this value!
+// If n is <= 0 then return the input text.
+
+// My Answer
+
 function encrypt(text, n) {
   if (n <= 0) return text;
   if (text == null) return null;
@@ -44,4 +62,21 @@ function decrypt(text, n) {
   decrypted = []
   }
   return text;
+}
+
+// Best Answer
+
+function encrypt(text, n) {
+  for (let i = 0; i < n; i++) {
+    text = text && text.replace(/.(.|$)/g, '$1') + text.replace(/(.)./g, '$1')
+  }
+  return text
+}
+
+function decrypt(text, n) {
+  let l = text && text.length / 2 | 0
+  for (let i = 0; i < n; i++) {
+    text = text.slice(l).replace(/./g, (_, i) => _ + (i < l ? text[i] : ''))
+  }
+  return text
 }
