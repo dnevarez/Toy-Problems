0256ea561187a900c5e45e008736ace3a7fe882f
Dan Nevarez <dnev.dev@gmail.com>
2017-03-27 14:57:57 -0600

    staircase

---
+++ javascript/codeChallenge/staircase.js
@@ -0,0 +1,13 @@
+// Consider a staircase of size n = 4:
+//
+//      #
+//     ##
+//    ###
+//   ####
+//
+// Observe that its base and height are both equal to n, and the image is drawn
+// using # symbols and spaces. The last line is not preceded by any spaces.
+
+// The staircase is right-aligned.
+
+// Write a function that takes in one parameter n and prints a staircase.
